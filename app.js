0b1972a3eb26cd824c83597d392b14cbe0258e6a
Blessing <31315580+blessingsunday1995@users.noreply.github.com>
2022-07-08 09:52:02 +0100

    .

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const api = {
   const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
+let body = document.getElementById('body')
+
+
+
 function setQuery(evt) {
   if (evt.keyCode == 13) {
      getResults(searchbox.value);
@@ -31,11 +35,27 @@ console.log(weather);
     let date = document.querySelector('.location .date');
     date.innerText = dateBuilder(now);
   
+const weatherState = weather.weather[0].main;
+
     let temp = document.querySelector('.current .temp');
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
   
     let weather_el = document.querySelector('.current .weather');
-    weather_el.innerText = weather.weather[0].main;
+    weather_el.innerText = weatherState;
+
+
+console.log(weatherState)
+
+     if(weatherState == 'Clouds'){
+            console.log("it working")
+            document.body.style.backgroundImage ="url('/bg.jpg')"
+    }else if(weatherState == 'Sunny'){
+
+        document.body.style.backgroundImage ="url('/bg.jpg')"
+
+    }
+ 
+
   
     let hilow = document.querySelector('.hi-low');
     hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
